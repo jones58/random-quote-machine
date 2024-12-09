87d56b023ec16d788a797b0660ac902c9b959af1
jones58 <108466184+jones58@users.noreply.github.com>
2024-12-09 14:14:37 +0000

    edit colours and styling

--- src/App.tsx
+++ src/App.tsx
@@ -23,45 +23,37 @@ const quotes = [
   },
 ];
 
-const colors = [
-  "#6B705C", // Muted Olive Green
-  "#B7B7A4", // Pale Taupe
-  "#A5A58D", // Warm Gray
-  "#CB997E", // Muted Peach
-  "#DDBEA9", // Soft Blush
-  "#FFE8D6", // Pale Cream
-  "#9CA3AF", // Muted Slate Gray
-  "#6C757D", // Muted Steel Gray
-];
-
+const colors = ["#d4d4d4", "#b4b4b4", "#909090", "#636363", "#494848"];
 function App() {
   const getNewQuote = () => {
     return quotes[Math.floor(Math.random() * quotes.length)];
   };
+  document.body.style.backgroundColor =
+    colors[Math.floor(Math.random() * colors.length)];
   const [quote, setQuote] = useState(getNewQuote);
 
   const changeQuote = () => {
     setQuote(getNewQuote());
-    document.body.style.backgroundColor =
-      colors[Math.floor(Math.random() * colors.length)];
   };
 
   return (
     <>
-      <div id="quote-box">
-        <div id="text">{quote.text}</div>
-        <div id="author">{quote.author}</div>
+      <div id="quote-box" className="App">
+        <h1 id="text">{quote.text}</h1>
+        <p id="author">{quote.author}</p>
         <div className="buttons">
-          <a
-            id="tweet-quote"
-            href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
-              `"${quote.text}" (${quote.author})`
-            )}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Tweet Quote
-          </a>
+          <button>
+            <a
+              href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+                `"${quote.text}" (${quote.author})`
+              )}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              id="tweet-quote"
+            >
+              Tweet Quote
+            </a>
+          </button>
           <button id="new-quote" onClick={changeQuote}>
             New Quote
           </button>
